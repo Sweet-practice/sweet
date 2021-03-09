2e20e54863da6291be181f486481df2cde6dd8c5
荒井春香 <>
2021-03-09 18:29:46 +0900

    order手続きページ作成

---
+++ resources/views/order_index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12 mt-3">
+            <h1>購入手続き</h1>
+        </div>
+
+        <div class="col-md-12 mt-3 p-5">
+            
+        </div>
+    </div>
+</div>
+@endsection
