ec1627509daf964d649a2cd4825e244d60145129
荒井春香 <>
2021-03-10 18:49:36 +0900

    Order関係作成

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Cart;
+use App\Sweet;
+use App\Order;
+use App\OrderDetail;
 
 class OrderController extends Controller
 {
@@ -13,7 +18,9 @@ class OrderController extends Controller
      */
     public function index()
     {
-        return view('order_index');
+        $orders = Order::where('user_id',Auth::user()->id)->get();
+        // print_r($orders);
+        return view('order_index',['orders' => $orders]);
     }
 
     /**
@@ -23,7 +30,8 @@ class OrderController extends Controller
      */
     public function create()
     {
-        //
+        $carts = Cart::where('user_id',Auth::user()->id)->get();
+        return view('order_create',['carts' => $carts]);
     }
 
     /**
@@ -34,7 +42,27 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $carts = Cart::where('user_id',Auth::user()->id)->get();
+
+        $order = new Order;
+        $order->user_id = Auth::user()->id;
+        $order->postage = $request->postage;
+        $order->total_price = $request->total_price;
+        $order->save();
+        $orderId = $order->id;
+
+        foreach ($carts as $cart){
+            $order_detail = new OrderDetail;
+            $order_detail->order_id = $order->id;
+            $order_detail->sweet_id = $cart->sweet->id;
+            $order_detail->sweet_name = $cart->sweet->name;
+            $order_detail->amout = $cart->amout;
+            $order_detail->price = $cart->amout*$cart->sweet->price;
+            $order_detail->save();
+        }
+
+        Cart::where('user_id',Auth::user()->id)->delete();
+        return view('home');
     }
 
     /**
@@ -43,9 +71,9 @@ class OrderController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Order $order)
     {
-        //
+        return view('order_show');
     }
 
     /**
--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['user_id', 'postage', 'total_price'];
+
+    public function order_detail()
+  {
+      return $this->hasMany('App\OrderDetail');
+  }
 }
