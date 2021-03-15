c89784a5e4c011d5536456acf3dd7e66f56b89e4
荒井春香 <>
2021-03-15 11:28:24 +0900

    変更

--- app/Favolite.php
+++ app/Favolite.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favolite extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+     //いいねしている投稿
+    public function sweet()
+    {
+        return $this->belongsTo('App\Sweet');
+    }
+
+    //いいねが既にされているかを確認
+    public function like_exist($id, $sweet_id)
+    {
+        //Likesテーブルのレコードにユーザーidと投稿idが一致するものを取得
+        $exist = Favolite::where('user_id', '=', $id)->where('sweet_id', '=', $sweet_id)->get();
+
+        // レコード（$exist）が存在するなら
+        if (!$exist->isEmpty()) {
+            return true;
+        } else {
+        // レコード（$exist）が存在しないなら
+            return false;
+        }
+    }
 }
